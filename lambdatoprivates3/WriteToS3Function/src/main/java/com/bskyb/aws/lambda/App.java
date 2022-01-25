ec1194bb710ef57d36e678a85f522db54d84594d
Jitendra <Soni>
2022-01-25 23:33:16 +0000

    written to s3 only

--- lambdatoprivates3/WriteToS3Function/src/main/java/com/bskyb/aws/lambda/App.java
+++ lambdatoprivates3/WriteToS3Function/src/main/java/com/bskyb/aws/lambda/App.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -12,37 +14,48 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.bskyb.aws.lambda.factory.AwsObjectFactory;
+import org.joda.time.DateTime;
+import org.joda.time.LocalTime;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 /**
  * Handler for requests to Lambda function.
  */
-public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+public class App implements RequestHandler<String, Map<String, Object>> {
+    private final S3Client s3Client;
 
-    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/json");
-        headers.put("X-Custom-Header", "application/json");
+    private final String BUCKET_NAME = System.getenv("RESULTS_BUCKET");
 
-        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
-                .withHeaders(headers);
-        try {
-            final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
-            String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
-
-            return response
-                    .withStatusCode(200)
-                    .withBody(output);
-        } catch (IOException e) {
-            return response
-                    .withBody("{}")
-                    .withStatusCode(500);
-        }
+    public App() {
+        this.s3Client = AwsObjectFactory.s3client();
     }
 
-    private String getPageContents(String address) throws IOException{
-        URL url = new URL(address);
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
-            return br.lines().collect(Collectors.joining(System.lineSeparator()));
-        }
+    public Map<String, Object> handleRequest(final String input, final Context context) {
+        String fileName = "Results" + DateTime.now();
+
+        String fileContent = input + " written at " + DateTime.now();
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(BUCKET_NAME)
+                .key(fileName)
+                .build();
+
+        this.s3Client.putObject(putObjectRequest, RequestBody.fromString(fileContent));
+
+        /*
+        Side effects
+        generates additional classes
+        non-static method holds reference of the object and cannot be garbage collected
+        Map<String, Object> map = new HashMap<String, Object>() {{ put(putObjectRequest.bucket(), putObjectRequest.key()); }};
+        */
+
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        map.put(putObjectRequest.bucket(), putObjectRequest.key());
+
+        return map;
     }
 }
