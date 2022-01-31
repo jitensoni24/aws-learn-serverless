bf0ca490b25d530a3f70a927be49098f5b1a7b22
Jitendra <Soni>
2022-01-31 23:59:23 +0000

    lambda with pojo

---
+++ orelly/HelloWorldLambdaJava/src/main/java/book/HelloWorld.java
@@ -0,0 +1,33 @@
+package book;
+
+public class HelloWorld {
+
+    public PojoResponse handlerPojo(PojoRequest request) {
+
+        return new PojoResponse("hello " + request.getS());
+    }
+
+    public static class PojoRequest {
+        private String s;
+
+        public String getS() {
+            return s;
+        }
+
+        public void setS(String s) {
+            this.s = s;
+        }
+    }
+
+    public static class PojoResponse {
+        private final String d;
+
+        public PojoResponse(String d) {
+            this.d = d;
+        }
+
+        public String getD() {
+            return d;
+        }
+    }
+}
